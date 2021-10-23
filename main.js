4b64fb9d427a09bf10847d3e5eafb73f4815ffa0
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-10-23 11:14:32 -0500

    Constantes, mensajes consola 23 10 2021

---
+++ main.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", ()=>{
+    const gravedad = 9.807;
+    let unidad = "m/s²";
+    console.group("Mensaje Constantes, Variable");
+        console.log(`%cLa grabedad de la tierra es: ${gravedad} ${unidad}`,`font-family:Arial; color:#fff;  background: red; padding:10px; border-radius:10px; text-transform: uppercase;font-weight:700;`);
+    console.groupEnd();
+
+});
+
+
+
+
+
+
